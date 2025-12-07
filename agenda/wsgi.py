2d7426bbcdb738a5eed13964842e93e1d7a35527
montesinosquisco123 <monelisa980@gmail.com>
2025-12-07 00:35:53 -0300

    Update wsgi.py

--- agenda/wsgi.py
+++ agenda/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for agenda project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = "/home/carolinamontes/agenda-pacientes"
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "agenda_pacientes.settings"
+)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agenda.settings')
-
 application = get_wsgi_application()
